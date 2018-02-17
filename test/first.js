1bf4221b7d948d70b13eb0d53e19a957a2e02319
kenneth-atiapa <kennethatiap@gmail.com>
2018-02-16 22:56:12 -0800

    adding test

---
+++ test/first.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha');
+var expect = require('chai').expect;
+
+
+describe("#some demo",()=>{
+
+    it("first demo app",()=>{
+        expect(true).to.be.true;
+    })
+
+});
